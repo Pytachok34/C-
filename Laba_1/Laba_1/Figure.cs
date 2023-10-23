6aa581d23f8631c0b35fe06cfc5d3ad7e99ea568
Pytachok34 <132889878+Pytachok34@users.noreply.github.com>
2023-10-23 12:38:58 +0300

    Add files via upload

---
+++ Laba_1/Laba_1/Figure.cs
@@ -0,0 +1,107 @@
+﻿using System;
+public abstract class Figure
+{
+    public abstract double Area();
+    public abstract double Perimeter();
+}
+public class Circle : Figure
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        if (radius <= 0)
+            throw new ArgumentException("Невозможно построить");
+        else
+            this.radius = radius;
+    }
+
+    public override double Area()
+    {
+        return Math.PI * radius * radius;
+    }
+
+    public override double Perimeter()
+    {
+        return 2 * Math.PI * radius;
+    }
+}
+
+public class Square : Figure
+{
+    private double side;
+
+    public Square(double side)
+    {
+        if (side <= 0)
+            throw new ArgumentException("Невозможно построить");
+        else
+            this.side = side;
+    }
+    public override double Area()
+    {
+        return side * side;
+    }
+
+    public override double Perimeter()
+    {
+        return 4 * side;
+    }
+}
+
+public class Rectangle : Figure
+{
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width)
+    {
+        if (length <= 0 || width <= 0)
+            throw new ArgumentException("Невозможно построить");
+        else
+        {
+            this.length = length;
+            this.width = width;
+        }
+    }
+
+    public override double Area()
+    {
+        return length * width;
+    }
+
+    public override double Perimeter()
+    {
+        return 2 * (length + width);
+    }
+}
+
+public class Triangle : Figure
+{
+    public double side1 { get; private set; }
+    private double side2;
+    private double side3;
+
+    public Triangle(double side1, double side2, double side3)
+    {
+        if (side1 <= 0 || side2 <= 0 || side3 <= 0 || side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1)
+            throw new ArgumentException("Невозможно построить");
+        else
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+            this.side3 = side3;
+        }
+    }
+
+    public override double Area()
+    {
+        double s = (side1 + side2 + side3) / 2;
+        return Math.Sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    }
+
+    public override double Perimeter()
+    {
+        return side1 + side2 + side3;
+    }
+}
---
+++ Laba_1/Laba_1/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LinkedListNode<T>
+{
+    public T Value { get; set; }
+    public LinkedListNode<T> Next { get; set; }
+
+    public LinkedListNode(T value)
+    {
+        Value = value;
+        Next = null;
+    }
+}
+
+public class MyLinkedList<T>
+{
+    private LinkedListNode<T> head;
+    private LinkedListNode<T> tail;
+
+    public void AddToHead(T value)
+    {
+        var newNode = new LinkedListNode<T>(value);
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            newNode.Next = head;
+            head = newNode;
+        }
+    }
+
+    public T DelFromHead()
+    {
+        if (head == null)
+        {
+            throw new InvalidOperationException("Список пуст");
+        }
+
+        var value = head.Value;
+        head = head.Next;
+        if (head == null)
+        {
+            tail = null;
+        }
+        return value;
+    }
+
+    public T Get_head_Value()
+    {
+        if (head == null)
+        {
+            throw new InvalidOperationException("Список пуст");
+        }
+        return head.Value;
+    }
+
+    public MyLinkedList<T> GetTail()
+    {
+        var newList = new MyLinkedList<T>();
+        if (head != null)
+        {
+            newList.head = head.Next;
+            newList.tail = tail;
+        }
+        return newList;
+    }
+    public LinkedListNode<T> get_head()
+    {
+        if (head == null)
+        {
+            throw new InvalidOperationException("Список пуст");
+        }
+        return head;
+    }
+}
+
+class Program
+{
+    static void Main()
+    { 
+        var intList = new MyLinkedList<int>();
+        intList.AddToHead(1);
+        intList.AddToHead(2);
+        intList.AddToHead(3);
+        LinkedListNode<int> int_ptr = intList.get_head();
+        while(int_ptr!= null)
+        {
+            Console.WriteLine(int_ptr.Value);
+            int_ptr= int_ptr.Next;
+        }
+        var stringList = new MyLinkedList<string>();
+        stringList.AddToHead("This is first string");
+        stringList.AddToHead("This is second string");
+        stringList.AddToHead("This is third string, but it could be first");
+        LinkedListNode<string> string_ptr = stringList.get_head();
+        while (string_ptr != null)
+        {
+            Console.WriteLine(string_ptr.Value);
+            string_ptr = string_ptr.Next;
+        }
+        var figureList = new MyLinkedList<Figure>();
+        figureList.AddToHead(new Circle(10));
+        figureList.AddToHead(new Triangle(2, 4, 5));
+        figureList.AddToHead(new Square(3));
+        figureList.AddToHead(new Rectangle(7, 6));
+        LinkedListNode<Figure> figure_ptr = figureList.get_head();
+        while(figure_ptr != null)
+        {
+            Console.WriteLine($"Площадь: { figure_ptr.Value.Area()}, Периметр: { figure_ptr.Value.Perimeter()}");
+            figure_ptr = figure_ptr.Next;
+        }
+    }
+}
